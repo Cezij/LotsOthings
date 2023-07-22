6bbf6358bc8348c3fbaaaca1c422e76f58a46ca7
gorcd <gorcdavid@gmail.com>
2023-07-22 12:06:09 +0200

    valpha

--- src/main/java/com/cezij/lots/mixin/TitleScreenMixin.java
+++ src/main/java/com/cezij/lots/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package com.cezij.lots.mixin;
 
-import com.example.example_mod.ExampleMod;
+import com.cezij.lots.lots;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		lots.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
